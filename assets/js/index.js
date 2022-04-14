ece0804769389f6399212fab9893bfbd242ed2b4
Kirill <freshjs.ftd.@gmail.com>
2022-04-14 20:18:18 +0300

    Add then error callback example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const response = fetch('./assets/js/data.json')
-  .then((response) => response.json())
-  .then((data) => {
-    state.users = data;
-  });
+fetch('./assets/js/data.json')
+  .then(
+    (response) => response.json(),
+    (error) => {
+      console.log('error on server');
+    }
+  )
+  .then(
+    (data) => {
+      state.users = data;
+    },
+    (error) => {
+      console.log('error while parsing');
+    }
+  );
 
 const state = {};
